333d0a562fc4e9fa5bd0544b58c47e35a2bece8d
djdu4 <djdu4496@gmail.com>
2022-10-12 17:21:36 +0900

    6_23. 기본형 매개변수 ~ 6_37. this

---
+++ src/Ex6_7.java
@@ -0,0 +1,27 @@
+class Data2 { int x; }
+
+
+public class Ex6_7 {
+    public static void main(String[] args) {
+    	Data2 d = new Data2();
+    	
+    	d.x = 10;
+    	System.out.println("main() : x = " + d.x);
+    	
+    	change(d);
+    	System.out.println("After change(d)");
+    	System.out.println("main() : x = " + d.x);
+    }
+    
+    static void change(Data2 d) {
+    	d.x = 1000;
+    	System.out.println("change() : x = " + d.x);
+    }
+}
+
+/*
+	main() : x = 10
+	change() : x = 1000
+	After change(d)
+	main() : x = 1000
+*/
