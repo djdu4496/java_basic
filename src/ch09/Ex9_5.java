068fcda5b03b67fa0453df41458e78163c6ccddf
djdu4496 <djdu4496@gmail.com>
2022-10-27 23:48:30 +0900

    Ex9_9 ~ Ex9_12

--- src/ch09/Ex9_5.java
+++ src/ch09/Ex9_5.java
@@ -1,14 +1,14 @@
 package ch09;
 
-class Card {
+class Card2 {
     String kind;
     int number;
 
-    Card() {
+    Card2() {
         this("SPADE", 1);  // Card(String kind, int number)를 호출
     }
 
-    Card(String kind, int number) {
+    Card2(String kind, int number) {
         this.kind = kind;
         this.number = number;
     }
@@ -21,8 +21,8 @@ class Card {
 
 class Ex9_5 {
     public static void main(String[] args) {
-        Card c1 = new Card();
-        Card c2 = new Card("HEART", 10);
+        Card2 c1 = new Card2();
+        Card2 c2 = new Card2("HEART", 10);
         System.out.println(c1.toString());
         System.out.println(c2.toString());
     }
