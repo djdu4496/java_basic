2d49de4b27ea0989bb1f936e735220bfd3a9fa7e
djdu4496 <djdu4496@gmail.com>
2022-11-14 19:04:31 +0900

    Ex13_9(2)

--- src/ch06/Ex6_1.java
+++ src/ch06/Ex6_1.java
@@ -6,18 +6,18 @@ public class Ex6_1 {
     	t = new Tv();
     	t.channel = 7;
     	t.channelDown();
-    	System.out.println("���� ä���� " + t.channel + " �Դϴ�.");
+    	System.out.println("현재 채널은 " + t.channel + " 입니다.");
     }
 }
 
 class Tv {
-	// ch06.Tv�� �Ӽ�(�������)
-	String color;    // ����
-	boolean power;   // ��������(on/off)
-	int channel;     // ä��
-	
-	// ch06.Tv�� ���(�޼���)
-	void power()       { power = !power; }
-	void channelUp()   { ++channel; }
-	void channelDown() { --channel; }
+	// Tv 속성(멤버변수)
+	String color;                       // 색상
+	boolean power;                      // 전원상태 (on/off)
+	int channel;                        // 채널
+
+	// TV 기능(메서드)
+	void power() { power = !power; }    // TV를 켜거나 끄는 기능을 하는 메서드
+	void channelUp() { ++channel; }     // TV 채널을 높이는 기능을 하는 메서드
+	void channelDown() { --channel; }   // TV 채널을 낮추는 기능을 하는 메서드
 }
--- src/ch07/Ex7_7.java
+++ src/ch07/Ex7_7.java
@@ -2,15 +2,18 @@ package ch07;
 
 class Ex7_7 {
 	public static void main(String[] args) {
-		Car car = null;
-		FireEngine fe = new FireEngine();
-		FireEngine fe2 = null;
+		Car car = new Car();
+		FireEngine fe = (FireEngine)car;  // 형변환 런타임 에러. java.lang.ClassCastException
+		fe.water();  // 컴파일 OK.
+
+
+//		FireEngine fe2 = null;
 		
-		fe.water();
-		car = fe;     // car = (Car)fe;���� ����ȯ�� ������
-//		car.water();  // ������ ����!!! CarŸ���� ���������δ� water()�� ȣ���� �� ����.
-		fe2 = (FireEngine)car; // �ڼ�Ÿ�� <- ����Ÿ��. ����ȯ ���� �Ұ�
-		fe2.water();
+//		fe.water();
+//		car = fe;     // car = (Car)fe;에서 형변환이 생략됨
+////		car.water();  // 컴파일 에러!!! Car타입의 참조변수로는 water()를 호출할 수 없다.
+//		fe2 = (FireEngine)car; // 자손타입 ← 조상타입. 형변환 생략 불가
+//		fe2.water();
 	}
 } 
 
@@ -28,8 +31,8 @@ class Car {
 
 }
 
-class FireEngine extends Car { // �ҹ���
-	void water() {  // �� �Ѹ��� ���
+class FireEngine extends Car { // 소방차
+	void water() {  // 물 뿌리는 기능
 		System.out.println("water!!!");
 	}
 }
