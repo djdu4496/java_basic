29716a35148506a1a6e002f112f8d76e977f1c31
djdu4496 <djdu4496@gmail.com>
2022-11-15 21:45:32 +0900

    Ex6_3 ~ Ex6_4 수정

--- src/ch06/Ex6_3.java
+++ src/ch06/Ex6_3.java
@@ -6,21 +6,21 @@ public class Ex6_3 {
     	System.out.println("ch06.Card.height = " + Card.height);    // Ŭ��������(static����)�� ��ü�������� 'Ŭ�����̸�.Ŭ��������'�� ���� ��� �����ϴ�.
     	
     	Card c1 = new Card();
-    	c1.kind = "Heart";    // �ν��Ͻ������� ���� �����Ѵ�.    
-    	c1.number = 7;        // �ν��Ͻ������� ���� �����Ѵ�.
+    	c1.kind = "Heart";
+    	c1.number = 7;
     	
     	Card c2 = new Card();
-    	c2.kind = "Spade";    // �ν��Ͻ������� ���� �����Ѵ�.
-    	c2.number = 4;        // �ν��Ͻ������� ���� �����Ѵ�.
-    	
-    	System.out.println("c1�� " + c1.kind + ", " + c1.number + "�̸�, ũ��� (" + c1.width + ", " + c1.height + ")");
-    	System.out.println("c2�� " + c2.kind + ", " + c2.number + "�̸�, ũ��� (" + c2.width + ", " + c2.height + ")");
-    	System.out.println("c1�� width�� height�� ���� 50, 80���� �����մϴ�.");
-    	c1.width = 50;        // Ŭ���������� ���� �����Ѵ�.
-    	c1.height = 80;		  // Ŭ���������� ���� �����Ѵ�.
-    	
-    	System.out.println("c1�� " + c1.kind + ", " + c1.number + "�̸�, ũ��� (" + c1.width + ", " + c1.height + ")");
-    	System.out.println("c2�� " + c2.kind + ", " + c2.number + "�̸�, ũ��� (" + c2.width + ", " + c2.height + ")");
+    	c2.kind = "Spade";
+    	c2.number = 4;
+
+		System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
+		System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
+		System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+		Card.width = 50;
+		Card.height = 80;
+
+		System.out.println("c1은 " + c1.kind + ", " + c1.number + "이며, 크기는 (" + c1.width + ", " + c1.height + ")");
+		System.out.println("c2는 " + c2.kind + ", " + c2.number + "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
     }
 }
 
--- src/ch06/Ex6_4.java
+++ src/ch06/Ex6_4.java
@@ -2,13 +2,16 @@ package ch06;
 
 public class Ex6_4 {
     public static void main(String[] args) {
-    	MyMath mm = new MyMath();
-    	long result1 = mm.add(5L, 3L);
+    	MyMath mm = new MyMath();  // 2. MyMath인스턴스 생성
+
+		// 3. MyMath인스턴스 사용
+    	long result1 = mm.add(5L, 3L);         // add 메서드 호출
     	long result2 = mm.subtract(5L, 3L);
     	long result3 = mm.multiply(3L, 5L);
-    	double result4 = mm.divide(5L, 3L);    // double ��� long������ ȣ���Ͽ���. �� ���� double�� �ڵ�����ȯ�ȴ�.
-    	long result5 = mm.max(5L, 3L);
-    	long result6 = mm.min(5L, 3L);
+    	double result4 = mm.divide(5L, 3L);
+    	long result5 = mm.max(5L, 3L);  // 둘 중에 큰 값을 반환하는 메서드
+    	long result6 = mm.min(5L, 3L);  // 둘 중에 작은 값을 반환하는 메서드
+		mm.printGugudan(12);
     	
     	System.out.println("add(5L, 3L) = " + result1);
     	System.out.println("subtract(5L, 3L) = " + result2);
@@ -21,11 +24,21 @@ public class Ex6_4 {
     
 }
 
-class MyMath {
+class MyMath {  // 1. MyMath클래스 작성(메서드 작성)
+	void printGugudan(int dan) {
+		if(!(2<=dan && dan<=9))
+			return;  // 입력받은 단(dan)이 2~9가 아니면, 메서드 종료하고 돌아가기
+
+		for(int i = 1; i <= 9;i++) {
+			System.out.printf("%d * %d = %d%n", dan, i, dan * i);
+		}
+//		return;  // 반환타입이 void가 아닌 경우, 생략 가능(컴파일러 자동 추가)
+	}
+
 	long add(long a, long b) {
 		long result = a + b;
-		return result; 
-//		return a + b;    // ���� �� ���� �̿� ���� �� �ٷ� ������ �� �� �ִ�.
+		return result;   // 작업을 마치면 호출한 곳으로 돌아온다.
+//		return a + b;    // 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
 	}
 	long subtract(long a, long b) {
 		return a - b;
@@ -36,14 +49,15 @@ class MyMath {
 	double divide(double a, double b) {
 		return a / b;
 	}
-	// �� ���� �޾Ƽ� �� �߿� ū ���� ��ȯ�ϴ� �޼��带 �ۼ��Ͻÿ�.
+	// 두 값을 받아서 둘 중에 큰 값을 반환하는 메서드를 작성하시오.
 	long max(long a, long b) {
-//		long result = 0;
-//		result = a > b ? a : b;
-//		return result; 
+//		if(a>b)
+//			return a;  // 조건식이 참일 때만 실행
+//		else
+//			return b;  // 조건식이 거짓일 때만 실행
 		return a > b ? a : b;
 	}
-	// �� ���� �޾Ƽ� �� �߿� ���� ���� ��ȯ�ϴ� �޼��带 �ۼ��Ͻÿ�.
+	// 두 값을 받아서 둘 중에 작은 값을 반환하는 메서드를 작성하시오.
 	long min(long a, long b){ 
 		return a < b ? a : b;
 	}
