55c649414445d4ec708cf3b0dc999195aa834daf
djdu4496 <djdu4496@gmail.com>
2022-11-18 23:29:05 +0900

    Ex13_9 ~ 13_10

--- src/ch07/PointTest.java
+++ src/ch07/PointTest.java
@@ -3,44 +3,44 @@ package ch07;
 class Point4 {
 	int x;
 	int y;
-	
+
 	Point4(int x, int y) {
-		this.x = x;    // ������ �� - Object();  // ��� �����ڴ� ù �ٿ� �ٸ� �����ڸ� ȣ���ؾ� �Ѵ�.
+		this.x = x;    // 컴파일 후 - Object();  // 모든 생성자는 첫 줄에 다른 생성자를 호출해야 한다.
 		this.y = y;
 	}
-	
+
 	String getLocation() {
 		return "x : " + x + ", y : " + y;
 	}
 }
-// 1. ������ ������ super() �ۼ� �� �� ���
-//class ch07.Point4_3D extends ch07.Point4 {
+// 1. 조상의 생성자 super() 작성 안 할 경우
+//class Point4_3D extends Point4 {
 //	int z;
-//	
-//	ch07.Point4_3D(int x, int y, int z) {
-//		
+//
+//	Point4_3D(int x, int y, int z) {
+//
 //		this.x = x;
 //		this.y = y;
 //		this.z = z;
-//	}  // Implicit super constructor ch07.Point4() is undefined. Must explicitly invoke another constructor
-//	
+//	}  // Implicit super constructor Point4() is undefined. Must explicitly invoke another constructor
+//
 //	String getLocation() {
 //		return "x : " + x + ", y : " + y + ", z : " + z;
-//	}  // �������̵�
+//	}  // 오버라이딩
 //}
 
-// 2. ������ ������ super() �ۼ� �� ���
+// 2. 조상의 생성자 super() 작성 할 경우
 class Point4_3D extends Point4 {
 	int z;
-	
+
 	Point4_3D(int x, int y, int z) {
 		super(x, y);
 		this.z = z;
-	}  
-	
+	}
+
 	String getLocation() {
 		return "x : " + x + ", y : " + y + ", z : " + z;
-	}  // �������̵�
+	}  // 오버라이딩
 }
 public class PointTest {
 
@@ -51,10 +51,10 @@ public class PointTest {
 
 }
 
-/* ���  1. ������ ������ super() �ۼ� �� �� ���
- * // Implicit super constructor ch07.Point4() is undefined. Must explicitly invoke another constructor
+/* 출력  1. 조상의 생성자 super() 작성 안 할 경우
+ * // Implicit super constructor Point4() is undefined. Must explicitly invoke another constructor
  */
 
-/* ��� 2. ������ ������ super() �ۼ� �� ���
+/* 출력 2. 조상의 생성자 super() 작성 할 경우
  * x : 1, y : 2, z : 3
  */
--- src/ch13/Ex13_9.java
+++ src/ch13/Ex13_9.java
@@ -6,11 +6,14 @@ class Ex13_9 {
     public static void main(String[] args) throws Exception {
         ThreadEx9_1 th1 = new ThreadEx9_1();
         th1.start();
-
+        System.out.println("isInterrupted():"+ th1.isInterrupted()); // false
         String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
         System.out.println("입력하신 값은 " + input + "입니다.");
         th1.interrupt();  // interrupt()를 호출하면, interrupted상태가 true가 된다.
-        System.out.println("isInterrupted():"+ th1.isInterrupted()); // true
+//        System.out.println("isInterrupted():"+ th1.isInterrupted()); // true
+//        System.out.println("isInterrupted():"+ th1.isInterrupted()); // true
+//        // main메서드가 interrupt되었는지 확인
+//        System.out.println("interrupted():"+ Thread.interrupted()); // true
     }
 }
 
@@ -22,6 +25,12 @@ class ThreadEx9_1 extends Thread {
             System.out.println(i--);
             for(long x=0;x<2500000000L;x++); // 시간 지연
         }
+        System.out.println("isInterrupted():"+ this.isInterrupted()); // true
+        System.out.println("isInterrupted():"+ this.isInterrupted()); // true
+        // isInterrupted()와 달리 interrupted()는 interrupted라는 상태변수를 false로 초기화
+        System.out.println("interrupted():"+ Thread.interrupted()); // true
+        System.out.println("interrupted():"+ Thread.interrupted()); // false
+
         System.out.println("카운트가 종료되었습니다.");
     }
 }
