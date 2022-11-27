24329502cd29cb563ab50de8b9bafef93b0ba889
djdu4496 <djdu4496@gmail.com>
2022-11-27 23:28:59 +0900

    정석코딩 Day7 | 연습문제 생각정리 훈련 ~ Ex5_13

--- src/ver3/practice/ch05/Ex5_13.java
+++ src/ver3/practice/ch05/Ex5_13.java
@@ -1,5 +1,7 @@
 package ver3.practice.ch05;
 
+// 단어의 글자위치를 섞어서 보여주고 원래의 단어를 맞추는 예제이다. 실행결과와 같이 동작하도록 예제의 빈 곳을 채우시오.
+
 import java.util.Scanner;
 
 public class Ex5_13 {
@@ -18,10 +20,17 @@ public class Ex5_13 {
 
             // (1) 알맞은 코드를 넣어 완성하시오.
             // char배열 question에 담긴 문자의 위치를 임의로 바꾼다.
-            char tmp = ' ';
-            tmp = question[i];
-            question[i] = question[(int)(Math.random()*words.length + 1)];
-            question[(int)(Math.random()*words.length + 1)] = question[i];
+            int num = (int)(Math.random()*question.length + 1);  // 임의의 인덱스를 저장히기 위한 변수 num 선언 및 초기화
+            for(int j = 0; j < question.length; j++) {
+                char tmp = ' ';  // 임시변수 tmp 선언
+                
+                tmp = question[num];  // question[num]과 question[j] 문자를 교환한다.
+                question[num] = question[j];
+                question[j] = tmp;
+            }
+
+
+
 
             System.out.printf("Q%d. %s의 정답을 입력하세요.>", i + 1, new String(question));
 
