9cc836df7dc9814738e7606d9b84655a6782f921
djdu4496 <djdu4496@gmail.com>
2022-12-15 20:47:56 +0900

    정석코딩 Day25 | 연습문제 생각정리 훈련 Ex9_11 '유효성 검사'

--- src/ver3/practice/ch09/Ex9_11.java
+++ src/ver3/practice/ch09/Ex9_11.java
@@ -7,33 +7,37 @@ import java.util.StringTokenizer;
 
 public class Ex9_11 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         // 1. 두 숫자를 입력받는다.
-        StringTokenizer st = new StringTokenizer(sc.nextLine() + " ");
-        int num1 = Integer.parseInt(st.nextToken());
-        int num2 = Integer.parseInt(st.nextToken());
+        try {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
 
-
-        // 2. 유효성 검사
-
-        if(num1 < 2 || num2 > 9) {
-            System.out.println("단의 범위는 2와 9 사이의 값이어야 합니다.");
-            return;
-        }
-
-        // 3. num1단부터 num2단까지 출력
-            System.out.println("USAGE : GugudanTest " + num1 + " "+ num2);
-        for (int i = num1; i <= num2 ; i++) {
-            for (int j = 1; j <= 9; j++) {
-                System.out.printf("%d*%d=%d%n", i, j, i * j);
+            // 2. 유효성 검사
+            if(num1 > num2) {
+                int tmp = num1;
+                num1 = num2;
+                num2 = tmp;
             }
-            System.out.println();
+
+            if(num1 < 2 || num2 > 9) {
+                System.out.println("단의 범위는 2와 9 사이의 값이어야 합니다.");
+                System.out.println("USAGE : GugudanTest " + 3 + " " + 5);
+                return;
+            }
+
+            // 3. num1단부터 num2단까지 출력
+            for (int i = num1; i <= num2 ; i++) {
+                for (int j = 1; j <= 9; j++)
+                    System.out.printf("%d*%d=%d%n", i, j, i * j);
+                System.out.println();
+            }
+        } catch(ArrayIndexOutOfBoundsException ae) {
+            System.out.println("시작 단과 끝 단, 두 개의 정수를 입력해주세요.");
+            System.out.println("USAGE : GugudanTest " + 3 + " " + 5);
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
 
 // [실행결과]
-// 가나다
-//  가나다
-//   가나다
