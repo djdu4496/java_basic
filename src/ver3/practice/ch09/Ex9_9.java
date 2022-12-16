057bb32d12970bf186a66f01c4833a302ebd6301
djdu4496 <djdu4496@gmail.com>
2022-12-16 11:40:08 +0900

    정석코딩 Day26 | 연습문제 생각정리 훈련 Ex9_9

--- src/ver3/practice/ch09/Ex9_9.java
+++ src/ver3/practice/ch09/Ex9_9.java
@@ -12,18 +12,30 @@ public class Ex9_9 {
     /*
          (1) delChar메서드를 작성하시오.
     */
-    static String delChar(String src, String delCh) {
-        StringBuffer sb = new StringBuffer(src);
 
-        return "";
+    // 1. delCh을 순회한다. - for문 사용
+    // 2. delCh의 i번째 문자에 대해 src에 있는지 확인 - while문 사용(금지된 문자가 여러 개일 경우 처리)
+        // 2-1. 문자열 src의 내용 변경을 위해 StringBuffer를 생성한다.
+        // 2-2. 금지된 문자가 존재하는 경우, 해당 문자를 삭제한다. - indexOf(int ch) 사용
+        // 2-3. 금지문자를 제거한 문자열을 변수 src에 다시 저장한다. - 2-1. ~ 2-3. 반복
+        // 2-4. 금지문자가 문자열 src에 존재하지 않을 때까지 2-1 ~ 2-3 반복
+    // 3. 문자열 src를 반환한다.
+
+    static String delChar(String src, String delCh) {
+        for (int i = 0; i < delCh.length(); i++) {              // 1.
+            StringBuffer sb = new StringBuffer(src);            // 2-1.
+            while(src.indexOf(delCh.charAt(i)) != -1) {         // 2-4
+                sb.deleteCharAt(src.indexOf(delCh.charAt(i)));  // 2-2.
+                src = sb.toString();                            // 2-3.
+            }
+        }
+        return src;
     }
 
 
     public static void main(String[] args) {
         System.out.println("(1!2@3^4~5)" + " -> " + delChar("(1!2@3^4~5)","~!@#$%^&*()"));
-        System.out.println("(1 2   3   4\t5)" + " -> "
-         + delChar("(1 2   3  4\t5)","\t"));
-
+        System.out.println("(1 2 3 4\\t5)" + " -> " + delChar("(1 2 3 4\t5)"," \t"));
     }
 }
 
@@ -31,3 +43,6 @@ public class Ex9_9 {
 // (1!2@3^4~5) -> 12345
 // (1 2   3      5) -> (12345)
 // 레퍼런스
+// charAt은 알 겠는데 indexOf를 어떻게 활용해야 하는지 감이 잘 오지 않늗ㄴ다.
+// 이번엔 반복되는 걸 해결 X
+// 또 코드로 비벼서 풀었다. 생각정리 제대로 안 함
