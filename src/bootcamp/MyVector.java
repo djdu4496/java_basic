622148662eba62913830eccdc078b097339080a8
djdu4496 <djdu4496@gmail.com>
2022-12-21 16:12:37 +0900

    정석코딩 Day31 | [퀴즈] MyVectorTest 수정

--- src/bootcamp/MyVector.java
+++ src/bootcamp/MyVector.java
@@ -1,18 +1,24 @@
 package bootcamp;
 
+import java.util.StringJoiner;
+
 public class MyVector {
     Object[] objArr;  // Q2-1. 객체 배열 Object[] 선언
     int size;         //
     int capacity;
 
-    // Q2-2. 기본생성자 추가 [질문] 기본생성자 생성하면 capacity가 16인거 아닌가?
+    // Q2-2. 기본생성자 추가
     MyVector() {
         this(16);  // 기존 생성자 호출
     }
+    // [질문] 기본생성자 생성하면 capacity가 16인거 아닌가? - 내가 iv 초기화를 안 해서 생긴 문제였음
 
     // Q2-2. 매개변수 있는 생성자 추가
     MyVector(int capacity) {
-        objArr = new Object[capacity];  // 변수 objArr에 길이가 capacity인 새로운 객체를 생성하여 저장
+        this.capacity = capacity;               // 매개변수 capacity값은 iv에 저장
+        objArr = new Object[capacity];          // 변수 objArr에 길이가 capacity인 새로운 객체를 생성하여 저장
+//      objArr[0] = null;
+//      size=1;
     }
 
     // Q2-3. 객체의 개수를 저장하기 위한 인스턴스 변수 size를 추가하고 반환
@@ -25,57 +31,156 @@ public class MyVector {
         return objArr.length;
     }
 
-    // Q2-3. MyVector클래스의 객체배열 objArr이 비었는지 확인
+    // Q2-3. MyVector클래스의 객체배열 objArr이 비었는지 확인 - size가 0인지 여부를 반환
     boolean isEmpty() {
         return size == 0;
     }
 
     // Q2-4. MyVector클래스의 객체배열 objArr에 객체를 추가
+    // 객체 obj를 마지막에 추가
+    // 1. 저장공간이 있나?
+        // 1-1. Yes -> (1) 저장
+        //             (2) size 변경
+        // 1-2. No ->  (1) 저장공간 확보 - capacity 변경
+        //             (2) 저장
+        //             (3) 저장된 객체 갯수 변경 - size 변경
+
     void add(Object obj) {
-        if(isEmpty())
-            objArr[0] = obj;
-        else
-            objArr[size - 1] = obj;
+        if(size==capacity) {                                        // 1-2. 저장공간이 없을 때 - 배열을 복사하여 저장공간을 확보한다.
+            capacity = objArr.length * 2;                                // 0. MyVector의 용량을 2배로 변경
+            Object[] objArr2 = new Object[capacity];                     // 1. 길이가 큰 배열 생성
+            for (int i = 0; i < objArr.length; i++) {
+                objArr2[i] = objArr[i];
+            }                                                            // 2. 기존 배열 내용 복사
+            objArr = objArr2;                                            // 3. 참조 변경
+        }
+
+        objArr[size++] = obj;                                       // (2) 저장 + (3) size 변경
     }
 
     // Q2-5. MyVector클래스의 객체배열 objArr에 저장된 객체 반환
     Object get(int index) {
-        // 1. 유효성 검사 - index가 0보다 작거나 size보다 크거나 같을 때
-        if(index < 0 || index >= size) {
-            // 예외를 발생시킨다.
-            throw new ArrayIndexOutOfBoundsException("index : " + index);
+        if(index < 0 || index >= size) {                            // 1. 유효성 검사 - index가 0보다 작거나 size보다 크거나 같을 때
+            throw new ArrayIndexOutOfBoundsException("index : " + index);  // 예외를 발생시킨다.
         }
-        // 2. objArr의 index번째 요소 반환
-        return objArr[index];
+        return objArr[index];                                       // 2. objArr의 index번째 요소 반환
     }
 
     // Q2-6. MyVector클래스의 객체배열 objArr에 저장된 모든 객체를 문자열로 이어서 반환
-    public String toString() {
-        String str = "[";
-        for (int i = 0; i < objArr.length; i++) {
-            str = str + objArr[i] + ", ";
+    public String toString() {  // StringJoiner 써볼 것
+        // first try
+//      String str = "";                                            // 1. 문자열 저장 위한 변수 str 선언 및 빈 문자열로 초기화
+//      for (int i = 0; i < objArr.length; i++) {                   // 2. 배열 objArr의 길이만큼 반복문 수행
+//          str = str + objArr[i];                                      // 2-1. 모든 객체를 문자열로 잇기 위해 str에 i번째 요소를 누적해서 더한다.
+//      }
+//      return str;                                                 // 3. 문자열 str을 반환한다.
+
+
+        // second try
+        String str = "";                                            // 1. 문자열 저장 위한 변수 str 선언 및 빈 문자열로 초기화
+        for (int i = 0; i < objArr.length; i++) {                   // 2. 배열 objArr의 길이만큼 반복문 수행
+            if(i == objArr.length - 1)                                  // 2-1. objArr의 마지막 요소일 경우
+                str = str + objArr[i];                                      // ","를 포함하지 않고 str에 add()로 저장한 객체를 더해 다시 저장한다.
+            else                                                        // 2-2. objArr의 마지막 요소가 아닌 경우
+                str = str + objArr[i] + ",";                                // ","를 포함하여 str에 add()로 저장한 객체를 더해 다시 저장한다.
         }
-        str = str + "]";
-        return str;
+
+        String[] strArr = str.split(",");                     // 3. 문자열을 ','를 구분자로 나눠서 배열에 저장
+        StringJoiner sj =
+                new StringJoiner(",", "[", "]"); // 4. ','를 구분자로, []로 감싸서 문자열을 결합한다.
+        for(String s : strArr)                                      // 5. 배열 strArr에 대해 향상된 for문 사용
+            sj.add(s.toUpperCase());                                    // 5-1. StringJoiner에 문자열을 대문자로 변환하여 추가한다.
+
+        return sj.toString();                                       // 6. 결합한 문자열을 반환한다.
     }
 
     // Q2-7. MyVector클래스의 객체배열 objArr에서 지정된 객체가 저장되어 있는 위치(index)를 반환
-    int indexOf(Object obj) {
-        for (int i = 0; i < objArr.length; i++) {
-            if(obj.equals(objArr[i]))
-                return i;
-        }
-        return -1;
-    }
+    // feedback1. null일때 테스트
+    // feedback2.
+    //  if(obj==null){
+    //
+    //  } else {
+    //
+    //  }
 
-    // Q2-8. objArr에서 지정된 객체를 삭제
-    boolean remove(Object obj) {
-        for (int i = 0; i < objArr.length; i++) {
-            if(obj.equals(objArr[i])) {
-                objArr[i] = null;
-                return true;
+    int indexOf(Object obj) {
+        // first try
+//      for (int i = 0; i < objArr.length; i++) {                   // 1. 배열 objArr의 길이만큼 반복문 수행
+//          if(obj.equals(objArr[i]))                               // 2. 매개변수로 저장된 객체 obj가 objArr[i]와 일치할 때
+//              return i;                                           // 3. 해당 index를 반환한다.
+//      }
+//      return -1;                                                  // 4. 일치하는 객체가 존재하지 않을 경우 -1 반환
+
+
+        // 객체가 null인 경우는 두 가지
+            // 1. null을 직접 저장한 경우 - NPE가 발생하지 않게 해야 한다.
+            // 2. null 객체의 인덱스가 size보다 크거나 같은 경우 - NPE 예외 처리
+
+
+        // second try
+//      for (int i = 0; i < objArr.length; i++) {                   // 1. 배열 objArr의 길이만큼 반복문 수행
+//          if (obj.equals(objArr[i])) {                            // 2. 매개변수로 저장된 객체 obj가 objArr[i]와 일치할 때
+//              return i;                                           // 3. 해당 index를 반환한다.
+//          }
+//      }
+//      return -1;                                                  // 4. 일치하는 객체가 존재하지 않을 경우 -1 반환
+
+
+        // third try - NullPointerException이 계속 났던 사연
+//      for (int i = 0; i < objArr.length; i++) {                   // 1. 배열 objArr의 길이만큼 반복문 수행
+//             if (obj.equals(objArr[i])) {                            // 2. 매개변수로 저장된 객체 obj가 objArr[i]와 일치할 때
+//          ➝ if (null.equals(objArr[i])) {                           // 에러!!! null에 메서드를 사용하면 안 된다!
+//              if(obj == null) {
+//                  return i;
+//              }
+//               return i;                                           // 3. 해당 index를 반환한다.
+//          }
+//      }
+//      return -1;                                                  // 4. 일치하는 객체가 존재하지 않을 경우 -1 반환
+
+        // fourth try
+        for (int i = 0; i < size; i++) {                            // 1. 배열 objArr의 길이만큼 반복문 수행 - i < objArr.length -> size로 변경
+            if(objArr[i] == null || obj.equals(objArr[i])) {        // 2. 객체 obj가 null이거나, objArr[i]와 일치하는 경우
+                return i;                                           // 3. 해당 index를 반환한다.
             }
         }
-        return false;
+        return -1;                                                  // 4. 일치하는 객체가 존재하지 않을 경우 -1 반환
     }
+
+        // Q2-8. objArr에서 지정된 객체를 삭제 - indexOf()를 사용할 것
+        // feedback. 자연어로 주석을 다시 수정(추상화x)
+    boolean remove(Object obj){
+
+        // first try
+//        for (int i = 0; i < objArr.length; i++) {                 // 1. 배열 objArr의 길이만큼 반복문 수행
+//            if(obj.equals(objArr[i])) {                           // 2. 매개변수로 저장된 obj 객체가 배열 objArr의 i번째 객체와 일치할 경우
+//                objArr[i] = null;                                 // 3. objArr[i]에 null을 저장한다.
+//                return true;                                      // 4. 삭제 작업이 완료됐으니 true를 반환한다.
+//            }
+//        }
+//        return false;                                             // 5. 일치하는 객체를 찾지 못한 경우, false를 반환
+
+
+        // second try
+        // 매개변수로 받은 obj 객체가 존재하는지 확인 - indexOf()를 사용
+//      if (indexOf(obj) == -1)                                          // 1. 매개변수로 받은 객체의 인덱스가 존재하지 않을 경우
+//          return false;                                                   // 1-1. false 반환
+//      else {                                                          // 2. 매개변수로 받은 객체의 인덱스가 존재할 경우
+//          objArr[indexOf(obj)] = null;                                    // 2-1. 객체 삭제
+//          return true;                                                    // 2-2. true 반환
+//      }
+
+        // third try
+        if (indexOf(obj) == -1)                                          // 1. 매개변수로 받은 객체의 인덱스가 존재하지 않을 경우
+            return false;                                                   // 1-1. false 반환
+        else {                                                           // 2. 매개변수로 받은 객체의 인덱스가 존재할 경우
+            if(indexOf(obj) != size - 1)                                    // 2-1. 마지막 데이터가 아닌 경우
+                System.arraycopy(objArr, indexOf(obj) + 1, objArr, indexOf(obj), size - indexOf(obj) - 1);  // 2-1. 삭제할 데이터 아래의 데이터를 한 칸씩 위로 복사해서 삭제할 데이터를 덮어쓴다.
+            objArr[size - 1] = null;                                     // 3. 마지막 객체 null로 변경
+            size--;                                                      // 4. 데이터 개수가 줄었기 때문에 size 1 감소 처리
+            return true;                                                    // 2-2. true 반환
+        }
+
+    }
+
 }
--- src/bootcamp/MyVectorTest.java
+++ src/bootcamp/MyVectorTest.java
@@ -5,47 +5,129 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyVectorTest {
-    MyVector v = new MyVector();
+    MyVector v = new MyVector();                                // MyVector클래스의 객체를 생성, 기본 생성자로 인스턴스 초기화
 
     @Test
-    public void sizeTest() {
-        assertTrue(v.size()==0);
+    public void sizeTest() {                                    // Test 1. 추가된 객체가 없을 때, size가 0인지 테스트
+        assertTrue(v.size==0);                              // 1. 추가된 객체가 없을 때, size가 0인지 테스트
+        v.add("1");                                             // Test 2. add()를 사용하여 객체를 추가했을 때, size값도 같이 증가하는지 테스트
+        assertTrue(v.size==1);                              // 2. 객체를 한 개 추가했을 때, size값도 1 늘어나는지 테스트
     }
 
     @Test
-    public void capacityTest() { // 모르겠음
-        assertTrue(v.capacity==0);
+    public void capacityTest() {                                // Test 1. 객체를 배열 objArr의 길이 이상 추가했을 때, 저장공간이 자동으로 확보되는지 여부 테스트
+        assertTrue(v.capacity==16);                         // 1. MyVector클래스의 객체 생성 이후, 인스턴스가 초기화 되었을 때, capacity가 16인지 테스트
+        for (int i = 0; i < v.objArr.length; i++) {                 // 1. capacity + 1 만큼 반복문 수행
+            v.add((i + 1) + "");                                    // 2. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+        }                                                               // [질문] 반복문 안에 길이를 v.objArr.length + 1로 하면 왜 무한 루프가 발생하나요?
+        v.add("17");                                                // 3. add()를 사용하여 특정 객체 추가
+        assertTrue(v.capacity == 32);                       // 4. 더 이상의 저장공간이 없을 때, 용량을 자동으로 확보해주는지 여부 테스트
     }
 
     @Test
-    public void isEmptyTest() {
-        assertTrue(v.isEmpty());
+    public void isEmptyTest() {                                 // Test 1. 배열 objArr에 저장된 객체가 없을 때 isEmpty()가 true를 반환하는지 여부 테스트
+        assertTrue(v.size == 0);                            // 1. 저장된 객체가 하나도 없는지 여부 테스트
+        assertTrue(v.isEmpty());                                    // 2. 저장된 객체가 하나도 없을 때, isEmpty()가 false를 반환하는지 테스트
+                                                                // Test 2. 배열 objArr에 저장된 객체가 있을 때 isEmpty()가 false를 반환하는지 여부 테스트
+        v.add("1");                                                 // 1. add()를 사용하여 특정 객체 추가
+        assertTrue(v.size > 0);                             // 2. 저장된 객체가 하나 이상인지 테스트
+        assertTrue(!v.isEmpty());                                   // 3. 저장된 객체가 하나 이상일 때, v.isEmpty()가 false를 반환하는지 테스트
     }
 
     @Test
-    public void addTest() {
-        v.add("1");
-        assertTrue(v.get(0).equals("1"));
+    public void addTest() {                                     // Test 1. 객체를 추가했을 때, 해당 index에 객체가 제대로 저장되었는지 테스트
+        for (int i = 0; i < v.objArr.length; i++) {                 // 1. 배열 objArr의 길이만큼 반복문 수행
+            v.add((i + 1) + "");                                    // 2. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+            assertTrue(v.objArr[v.size - 1].equals((i + 1) + ""));  // 3. add메서드의 인자로 지정된 객체와 objArr의 index번째 요소가 일치하는지 equals()를 사용하여 테스트
+        }
+                                                                // Test 2. 객체를 추가했을 때, 해당 index에 객체가 제대로 저장되었는지 테스트
+        v.add("17");                                                // 1. 특정 객체 "17" 추가
+        assertTrue(v.objArr[v.size - 1].equals("17"));              // 2. 추가된 객체를 get()으로 반환한 값과 objArr의 index번째 요소 일치 여부 테스트
+        assertTrue(v.capacity == 32);                       // 3. 더 이상의 저장공간이 없을 때, 용량을 자동으로 확보해주는지 여부 테스트
     }
 
     @Test
-    public void getTest() {
-        v.add("1");
-        assertTrue(v.get(0).equals(v.objArr[0]));
+    public void getTest() {                                     // Test 1. 객체를 추가했을 때, 해당 index에 저장된 객체 반환 여부 테스트
+        for (int i = 0; i < v.objArr.length; i++) {                 // 1. 배열 objArr의 길이만큼 반복문 수행
+            v.add((i + 1) + "");                                    // 2. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+            assertTrue(v.get(i).equals(v.objArr[i]));               // 3. 추가된 객체를 get()으로 반환한 값과 objArr의 index번째 요소가 일치하는지 equals()를 사용하여 테스트
+        }
+    }
+
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void getExceptionTest() {
+        assertTrue(v.get(-1)==null);                   // Test 1. 존재할 수 없는 인덱스 objArr[-1]에 대해 예외가 발생하는지 테스트
+        assertTrue(v.get(10000)==null);                // Test 2. 존재할 수 없는 인덱스 objArr[10000]에 대해 예외가 발생하는지 테스트
     }
 
     @Test
-    public void toStringTest() {
-        v.add("1");
-        System.out.println(v.toString());
+    public void toStringTest() {                               // Test 1. toString()값과 직접 객체를 이은 값이 일치하는지 여부 테스트
+        // first try
+//        String str = "";                                           // 1. 문자열 저장 위한 변수 str 선언 및 빈 문자열로 초기화
+//        for (int i = 0; i < v.objArr.length; i++) {                // 2. 배열 objArr의 길이만큼 반복문 수행
+//            v.add(i + 1 + "");                                     // 3. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+//            str += i + 1 + "";                                     // 4. str에 add()로 저장한 객체를 더해 다시 저장한다.
+//        }
+//        assertTrue(v.toString().equals(str));                      // 5. str과 v.toString()의 반환값이 일치하는지 equals()를 사용하여 테스트
+
+        // second try                                          // Test 1. toString()값과 직접 객체를 이은 값이 일치하는지 여부 테스트
+        String str = "[";                                          // 1. 문자열 str 선언 및 "["으로 초기화
+        for (int i = 0; i < v.objArr.length; i++) {                // 2. 배열 objArr의 길이만큼 반복문 수행
+            v.add(i + 1 + "");                                     // 3. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+            if(i == v.objArr.length - 1)                           // 4. 배열 objArr의 마지막 요소인 경우
+                str += i + 1 + "";                                     // 4-1. ","를 포함하지 않고 str에 add()로 저장한 객체를 더해 다시 저장한다.
+            else
+                str += i + 1 + ",";                                    // 4-2. ","를 포함하여 str에 add()로 저장한 객체를 더해 다시 저장한다.
+        }
+        str = str +  "]";                                          // 5. str에 배열의 끝을 알리는 대괄호 "]"를 추가한다.
+        assertTrue(v.toString().equals(str));                      // 6. v.toString()의 반환값과 str이 일치하는지 여부를 테스트한다.
     }
 
     @Test
-    public void indexOf() {
+    public void indexOfTest() {
+                                                               // Test 1. indexOf()를 사용하여 저장한 객체의 인덱스를 반환할 수 있는지 여부 테스트
+        for (int i = 0; i < v.objArr.length; i++) {                // 1. 배열 objArr의 길이만큼 반복문 수행
+            v.add(i + 1 + "");                                     // 2. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+            assertTrue(v.indexOf(i + 1 + "") == i);        // 3. indexOf(Obj obj)가 저장된 객체의 인덱스를 반환하는지 테스트한다
+        }
 
+        assertTrue(v.indexOf("20") == -1);             // Test 3. 존재하지 않은 객체에 대해 indexOf()를 사용하면 -1을 반환하는지 테스트
     }
 
     @Test
-    public void remove() {
+    public void indexOfNPETest() {                        // Test 1. indexOf()를 사용하여 null을 저장한 객체의 인덱스를 반환할 수 있는지 여부 테스트
+        v.add(null);                                               // 1. add()를 사용하여 배열 objArr에 null 객체를 저장
+        v.add(null);                                               // 1. add()를 사용하여 배열 objArr에 null 객체를 저장
+        v.add(null);                                               // 1. add()를 사용하여 배열 objArr에 null 객체를 저장
+        assertTrue(v.indexOf(null) == 0);                  // 2. null객체의 인덱스가 0인지 확인
+    }
+
+    @Test
+    public void removeTest() {                                 // Test 1. remove()를 사용하여 저장한 객체를 삭제할 수 있는지 테스트
+        v.add("1");                                                // 1. "1", "2", "3", "4", "5" 객체를 add()를 사용하여 저장
+        v.add("2");
+        v.add("3");
+        v.add("4");
+        v.add("5");
+
+        assertTrue(v.size == 5);                           // 3. 객체 삭제 전 저장된 객체 갯수 확인
+        assertTrue(v.remove("2")==true);               // 4. objArr[1]을 삭제 - 수행 여부 테스트
+        assertTrue(v.size == 4);                           // 5. 객체 삭제 후 저장된 객체 갯수 확인
+    }
+
+    @Test
+    public void removeTest2() {                          // Test 1. remove()를 사용하여 저장한 객체를 삭제할 수 있는지 테스트
+        for (int i = 0; i < v.objArr.length; i++)                 // 1. 배열 objArr의 길이만큼 반복문 수행
+            v.add(i + 1 + "");                                    // 2. add()를 사용하여 배열 objArr의 길이만큼 객체를 저장
+
+        assertTrue(v.size == 16);                         // 3. 객체 삭제 전 저장된 객체 갯수 확인
+        assertTrue(v.remove("2"));                            // 4. objArr[1]을 삭제 - 수행 여부 테스트
+        assertTrue(v.size == 15);                         // 5. 객체 삭제 후 저장된 객체 갯수 확인
+    }
+
+    @Test
+    public void removeTest3() {                          // Test 2. 존재하지 않는 객체에 대해 remove()를 사용했을 때 false를 반환하는지 테스트
+
+        assertTrue(!v.remove("10qwdqwdwqd"));
     }
 }
